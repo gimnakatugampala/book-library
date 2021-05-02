5a755a85e1158a97b6c2fc944cdc224454d65df9
Gimna Katugampala <gimnakatugampala1@gmail.com>
2021-05-02 22:34:56 +0530

    done with the delte funtionality]

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 const app = Vue.createApp({
     data(){
         return{
-            books:[]
+            books:[],
+            removeBtn: 'btn btn-danger'
         
         }
     },
     methods:{
-      
-
+        handleClick(select){
+            fetch('http://localhost:3000/books')
+            .then(res => res.json())
+            .then(data => {
+                data.forEach(book => {
+                    if(select === book.id){
+                        document.getElementById(select).remove()
+                    }
+                })
+            })
+        }
     },
     mounted(){
             fetch('http://localhost:3000/books')
